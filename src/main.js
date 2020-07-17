25274993d34de012257c7a829c8ae22708245473
loregunner <dahara59@gmail.com>
2020-07-17 18:36:31 -0500

    cambio con responsive

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import data from './data/lol/lol.js';
 console.log(example, data);
 document.getElementById("btnabrir").addEventListener("click",function(){
     document.getElementsByClassName("fondo_transparente")[0].style.display="block" 
-       return false
+      
 })
 document.getElementsByClassName("modal_cerrar")[0].addEventListener("click",function(){
     document.getElementsByClassName("fondo_transparente")[0].style.display="none" 
