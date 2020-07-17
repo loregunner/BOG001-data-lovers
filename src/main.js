e8bbfa03c04d639576cc94bc4e4f08c53d6c541b
loregunner <dahara59@gmail.com>
2020-07-17 09:06:47 -0500

    copia 1

--- src/main.js
+++ src/main.js
@@ -4,3 +4,10 @@ import data from './data/lol/lol.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
 console.log(example, data);
+document.getElementById("btnabrir").addEventListener("click",function(){
+    document.getElementsByClassName("fondo_transparente")[0].style.display="block" 
+       return false
+})
+document.getElementsByClassName("modal_cerrar")[0].addEventListener("click",function(){
+    document.getElementsByClassName("fondo_transparente")[0].style.display="none" 
+})
